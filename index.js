2e5475a82e64c77beeabfcc22f0162eac84684ad
edizonchavi <edizonchavi@gmail.com>
2023-02-13 23:30:16 -0500

    se solucionan los retos

--- index.js
+++ index.js
@@ -1,57 +1,34 @@
-/*
-Jr Fullstack Developer Test - Webcat
+import $t from './libs/test.js';
+import $t2 from './libs/1-source-data.js';
+import _ from 'lodash';
 
-Welcome to the Technical test for Jr Fullstack Developer
 
-We hope that everything is fully clear and understandable.
-However, if you have any questions, please send us an email
-to support@webcat.app with the subject "Jr Fullstack Test Questions"
-*/
-
-import $t from './libs/test.js'
-
-/*
-1. Data manipulation:
-  1. Transform the source data to the target data.
-  2. Return the target data.
-
-  Source data:
-    You can inspect the source data at /libs/1-source-data.js
-  Target Data:
-    {
-      balance: 1606400,
-      income: 3900000,
-      expenses: 2293600,
-      byCategories: {
-        Restaurants: -43600,
-        Income: 3900000,
-        Groceries: -250000,
-        Rent: -2000000
-      }
-    }
-
-  Hint: Use native array methods as well as
-    Lodash(https://lodash.com/docs) modules.
-*/
-import _ from 'lodash'
-const source = $t.source(1)
+//1
 $t.answer(1, async () => {
-  // Your code goes here
-  return 
-})
+  const market = $t2;
+  const result = market.reduce((acc, curr) => {
+    acc.balance += curr.category === 'Income' ? curr.amount : -curr.amount;
+    acc.income += curr.category === 'Income' ? curr.amount : 0;
+    acc.expenses += curr.category !== 'Income' ? curr.amount : 0;
+    acc.byCategories[curr.category] = (acc.byCategories[curr.category] || 0) + (curr.category === 'Income' ? curr.amount : -curr.amount);
+    return acc;
+  }, {
+    balance: 0,
+    income: 0,
+    expenses: 0,
+    byCategories: {},
+  });
 
-/*
-2. Asynchronous programming: 
-  1. First get the list of ids from the async function $source.getIds()
-  2. Then, for every id call the async function $source.getText(id) to get its text
-  3. Finally, return the list of resulting texts as an array.
-    
-*/
+  return result;
+});
+//2
 const $source = $t.source(2)
 $t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
+  const ids = await $source.getIds()
+
+  const promises = ids.map(id => $source.getText(id))
+  const texts = await Promise.all(promises)
+
+  
+  return texts
 })
